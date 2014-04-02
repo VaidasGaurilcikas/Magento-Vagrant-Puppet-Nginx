7a7640db35d2ff646713b4bbf3cefcd5740a5b3a
Vaidas Gaurilcikas <vaidas.gaurilcikas@gmail.com>
2014-04-02 15:26:01 +0100

    Fixed config

--- puppet/modules/thebox/manifests/config.pp
+++ puppet/modules/thebox/manifests/config.pp
@@ -1,6 +1,6 @@
 class thebox::config {
 	$hostname = 'box.localhost'
-	$dbpath = '/vagrant/db/bikes.sql'
+	$dbpath = '/vagrant/db/dev_db.sql'
 	$dbuser = 'dev_local'
 	$dbpass = 'dev_local'
 	$dbhost = 'localhost'
